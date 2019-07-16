09ea8069cd836bb549f6bbe987ec14e831867cb6
luisanrrango <luisanrrango@localhost>
2019-07-16 16:58:34 -0400

    Creación de ventana para el registro del Odontograma...y union de los Menus de Doctor

--- Swo_AplicacionesEJB/ejbModule/swo/model/manager/ManagerOdontograma.java
+++ Swo_AplicacionesEJB/ejbModule/swo/model/manager/ManagerOdontograma.java
@@ -1,5 +1,6 @@
 package swo.model.manager;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.LocalBean;
@@ -8,7 +9,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import swo.model.entities.SwoCara;
+import swo.model.entities.SwoDiente;
 import swo.model.entities.SwoOdontograma;
+import swo.model.entities.SwoPaciente;
+import swo.model.entities.SwoTratamiento;
 
 
 
@@ -46,10 +51,21 @@ private EntityManager em;
     }
     
     //metodo para insertar un Odontograma
-    public void insertarOdontograma(SwoOdontograma odontograma) throws Exception {
-  	  if(findSwoOdontogramaBycodigoate(odontograma.getCodigoAte())!=null)
-  		  throw new Exception("Ya existe el código indicado.");
-  	  em.persist(odontograma);
+    public void insertarOdontograma(int codpac, Date fecha_ate,String descripción_ate,int codtrata,int coddie, int codcar) throws Exception {
+  	SwoOdontograma odontograma=new SwoOdontograma();
+  	SwoPaciente paciente=buscarPorCodigoPac(codpac);
+  	SwoDiente diente=buscarPorCodigoDie(coddie);
+  	SwoCara cara=buscarPorCodigoCar(codcar);
+  	SwoTratamiento trata=buscarPorCodigoTrata(codtrata);
+  	
+    odontograma.setDescripcionAte(descripción_ate);
+  	odontograma.setFechaAte(fecha_ate);
+  	odontograma.setSwoTratamiento(trata);
+  	odontograma.setSwoPaciente(paciente);	
+   odontograma.setSwoDiente(diente);
+   odontograma.setSwoCara(cara);
+
+   em.persist(odontograma);
     }
     
     //métod para eliminar
@@ -69,5 +85,37 @@ private EntityManager em;
  // 	e.setSwoGenero(paciente.getSwoGenero());
   	  em.merge(e);
     }
+	 //Busca por codigo al Paciente
+	 public SwoPaciente buscarPorCodigoPac(int codPa) {
+		   SwoPaciente genero = em.find(SwoPaciente.class, codPa);
+		   return genero;
+}
+	 //Busca por codigo al diente
+	 public SwoDiente buscarPorCodigoDie(int codDie) {
+		   SwoDiente diente = em.find(SwoDiente.class, codDie);
+		   return diente;
+}
+	 //Busca por codigo al cara
+	 public SwoCara buscarPorCodigoCar(int codCar) {
+		   SwoCara cara = em.find(SwoCara.class, codCar);
+		   return cara;
+}
+	 //Busca por codigo al tratamiento
+	 public SwoTratamiento buscarPorCodigoTrata(int codtrat) {
+		   SwoTratamiento tratam = em.find(SwoTratamiento.class, codtrat);
+		   return tratam;
+}
+	// método de busqueda para el Odontograma
+		public SwoOdontograma findSwoOdontogramaBycodigoOdon(Integer codigo_Odonto) {
+			return em.find(SwoOdontograma.class, codigo_Odonto);
 
+		}
+		
+		// Método que retorna la lista de la tabla Pacientes
+		public List<SwoPaciente> listarPacie() {
+			String consulta = ("SELECT s FROM SwoPaciente s");
+			Query q = em.createQuery(consulta, SwoPaciente.class);
+			return q.getResultList();
+
+		}
 }
---
+++ Swo_AplicacionesWeb/src/swo/controller/BeanOdontograma.java
@@ -0,0 +1,155 @@
+package swo.controller;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import swo.model.entities.SwoOdontograma;
+import swo.model.entities.SwoPaciente;
+import swo.model.manager.ManagerOdontograma;
+import swo.model.manager.MangerPaciente;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class BeanOdontograma implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private ManagerOdontograma managerOdontograma;
+	private List<SwoOdontograma> listaOdontograma;
+	private List<SwoPaciente> listaPaciente;
+	private SwoPaciente paciente;
+	private SwoOdontograma odontograma;
+	private boolean panelColapsado;
+	private SwoPaciente pacienteSelecionado;
+	
+	//datos para insertar las claves foraneás
+	private String descripción_ate;
+	private Date fecha_ate;
+	private int codtrata;
+	private int coddie;
+	private int codcar;
+	private int codpac;
+	
+
+	@PostConstruct
+    public void inicializar() {
+	listaOdontograma =managerOdontograma.findAllOdontograma();
+	listaPaciente = managerOdontograma.listarPacie();
+	odontograma=new SwoOdontograma();
+	paciente=new SwoPaciente();
+	panelColapsado=true;
+	}
+	
+	public void actionListenerInsertarOdontograma() {
+		try {
+			managerOdontograma.insertarOdontograma(codpac, fecha_ate, descripción_ate, codtrata, coddie, codcar);
+			listaOdontograma=managerOdontograma.findAllOdontograma();
+			odontograma=new SwoOdontograma();
+			JSFUtil.crearMensajeInfo("Datos de Odontograma Insertados");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	public void actionListenerColapsarPanel() {
+		panelColapsado=!panelColapsado;
+	}
+	public void actionListenerSeleccinarPaciente(SwoPaciente paciente) {
+		pacienteSelecionado=paciente;
+	
+	}
+	public List<SwoOdontograma> getListaOdontograma() {
+		return listaOdontograma;
+	}
+	public void setListaOdontograma(List<SwoOdontograma> listaOdontograma) {
+		this.listaOdontograma = listaOdontograma;
+	}
+	public List<SwoPaciente> getListaPaciente() {
+		return listaPaciente;
+	}
+	public void setListaPaciente(List<SwoPaciente> listaPaciente) {
+		this.listaPaciente = listaPaciente;
+	}
+	public SwoPaciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(SwoPaciente paciente) {
+		this.paciente = paciente;
+	}
+	public SwoOdontograma getOdontograma() {
+		return odontograma;
+	}
+	public void setOdontograma(SwoOdontograma odontograma) {
+		this.odontograma = odontograma;
+	}
+	public boolean isPanelColapsado() {
+		return panelColapsado;
+	}
+	public void setPanelColapsado(boolean panelColapsado) {
+		this.panelColapsado = panelColapsado;
+	}
+
+	public String getDescripción_ate() {
+		return descripción_ate;
+	}
+
+	public void setDescripción_ate(String descripción_ate) {
+		this.descripción_ate = descripción_ate;
+	}
+
+	public Date getFecha_ate() {
+		return fecha_ate;
+	}
+
+	public void setFecha_ate(Date fecha_ate) {
+		this.fecha_ate = fecha_ate;
+	}
+
+	public int getCodtrata() {
+		return codtrata;
+	}
+
+	public void setCodtrata(int codtrata) {
+		this.codtrata = codtrata;
+	}
+
+	public int getCoddie() {
+		return coddie;
+	}
+
+	public void setCoddie(int coddie) {
+		this.coddie = coddie;
+	}
+
+	public int getCodcar() {
+		return codcar;
+	}
+
+	public void setCodcar(int codcar) {
+		this.codcar = codcar;
+	}
+
+	public int getCodpac() {
+		return codpac;
+	}
+
+	public void setCodpac(int codpac) {
+		this.codpac = codpac;
+	}
+
+	public SwoPaciente getPacienteSelecionado() {
+		return pacienteSelecionado;
+	}
+
+	public void setPacienteSelecionado(SwoPaciente pacienteSelecionado) {
+		this.pacienteSelecionado = pacienteSelecionado;
+	}
+	
+	
+}
